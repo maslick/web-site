2e5241bcb11b6a856dac3a5aa897b260629b524b
Pavel Maslov <pavel.maslov@cosylab.com>
2015-11-15 23:14:55 +0100

    * bootstrap angular * fonts * splash screen * grunt (web server)

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+    // Do grunt-related things in here
+    //grunt.loadNpmTasks('grunt-contrib-connect');
+
+
+    require('load-grunt-tasks')(grunt);
+
+
+    grunt.initConfig({
+        connect: {
+            server: {
+                options: {
+                    port: '3003',
+                    hostname: '0.0.0.0',
+                    keepalive: true,
+                    open: true
+                }
+            }
+        }
+    });
+
+
+
+    grunt.registerTask('dev', [
+        'connect:server'
+    ]);
+};
